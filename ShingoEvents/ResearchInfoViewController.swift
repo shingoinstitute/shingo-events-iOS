e0cb367219a418efcc1e1a0a70d2985248444bab
Craig Blackburn <cr.blackburn89@gmail.com>
2016-04-13 15:14:27 -0600

    Rich text format support for research info view

--- ShingoEvents/ResearchInfoViewController.swift
+++ ShingoEvents/ResearchInfoViewController.swift
@@ -12,12 +12,47 @@ class ResearchInfoViewController: UIViewController {
 
     @IBOutlet weak var bookImage: UIImageView!
     @IBOutlet weak var abstractTextField: UITextView!
-//    @IBOutlet weak var scrollView: UIScrollView!
+    var scrollView: UIScrollView = UIScrollView.newAutoLayoutView()
+    var backdrop = UIView.newAutoLayoutView()
     
     var recipient:Recipient!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let shingoBlue = UIColor(netHex: 0x002f56)
+        
+        bookImage.removeFromSuperview()
+        abstractTextField.removeFromSuperview()
+        view.addSubview(scrollView)
+        view.addSubview(backdrop)
+        scrollView.addSubview(bookImage)
+        scrollView.addSubview(abstractTextField)
+        
+        scrollView.autoPinToTopLayoutGuideOfViewController(self, withInset: 0)
+        scrollView.autoPinEdgeToSuperviewEdge(.Left)
+        scrollView.autoPinEdgeToSuperviewEdge(.Right)
+        scrollView.autoPinEdgeToSuperviewEdge(.Bottom)
+        
+        bookImage.autoPinEdgeToSuperviewEdge(.Top, withInset: 8)
+        bookImage.autoPinEdgeToSuperviewEdge(.Left, withInset: 8)
+        bookImage.autoPinEdgeToSuperviewEdge(.Right, withInset:  8)
+        bookImage.contentMode = .ScaleAspectFit
+        
+        abstractTextField.autoPinEdge(.Top, toEdge: .Bottom, ofView: bookImage, withOffset: 8)
+        abstractTextField.autoPinEdge(.Left, toEdge: .Left, ofView: view, withOffset: 0)
+        abstractTextField.autoPinEdge(.Right, toEdge: .Right, ofView: view, withOffset: 0)
+        abstractTextField.autoPinEdge(.Bottom, toEdge: .Bottom, ofView: scrollView, withOffset: 0)
+        abstractTextField.scrollEnabled = false
+        abstractTextField.textContainerInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        abstractTextField.backgroundColor = shingoBlue
+        
+        view.bringSubviewToFront(scrollView)
+        backdrop.autoPinEdge(.Top, toEdge: .Bottom, ofView: bookImage, withOffset: 8)
+        backdrop.autoPinEdgeToSuperviewEdge(.Left)
+        backdrop.autoPinEdgeToSuperviewEdge(.Right)
+        backdrop.autoPinEdgeToSuperviewEdge(.Bottom)
+        backdrop.backgroundColor = shingoBlue
         
         self.automaticallyAdjustsScrollViewInsets = false
 
@@ -28,55 +63,29 @@ class ResearchInfoViewController: UIViewController {
         }
         
         abstractTextField.text = ""
-//        if recipient.authors != nil && recipient.name != nil{
-//            if (recipient.authors.rangeOfString("&") != nil) {
-//                abstractTextField.text = "Presenting the authors of \"" + recipient.name + "\", " + recipient.authors + ", recipients of the Research Award.\n\n"
-//            } else {
-//                abstractTextField.text = "Presenting the author of \"" + recipient.name + "\", " + recipient.authors + ", recipient of the Research Award.\n\n"
-//            }
-//        } else {
-//            if recipient.authors != nil {
-//                if recipient.authors.rangeOfString(",") != nil {
-//                    abstractTextField.text! += "Presenting " + recipient.authors + ", recipients of the Research Award.\n\n"
-//                } else {
-//                    abstractTextField.text! += "Presenting " + recipient.authors + ", recipient of the Research Award.\n\n"
-//                }
-//            }
-//            
-//            if recipient.name != nil {
-//                abstractTextField.text! += "Presenting the book \"" + recipient.name + "\", for being rewarded the Research Award.\n\n"
-//            }
-//        }
         
-        if recipient.abstract != nil {
+        if recipient.richAbstract != nil {
+            
+            do {
+                let htmlString: String! = "<style>body{color:white;}</style><font size=\"5\">" + recipient.richAbstract! + "</font></style>";
+                abstractTextField.attributedText = try NSAttributedString(data: htmlString.dataUsingEncoding(NSUTF8StringEncoding)!,
+                                                                      options: [NSDocumentTypeDocumentAttribute : NSHTMLTextDocumentType],
+                                                                      documentAttributes: nil)
+            } catch {
+                // do nothing
+            }
+            
+        } else if recipient.abstract != nil {
             abstractTextField.text! += recipient.abstract
         } else {
             abstractTextField.text! += "Book description coming soon."
         }
-
-        
-
         
 //        var frame:CGRect = abstractTextField.frame
 //        frame.size.height = abstractTextField.contentSize.height
 //        abstractTextField.frame = frame
 //        abstractTextField.scrollEnabled = false
-//        calculateScrollViewContent()
     }
     
-//    func calculateScrollViewContent() {
-//        var height:CGFloat = 0
-//        let uiOffset:CGFloat = 8.0
-//        
-//        abstractTextField.sizeToFit()
-//        abstractTextField.layoutIfNeeded()
-//        
-//        height += bookImage.frame.height
-//        height += abstractTextField.contentSize.height
-//        height += (uiOffset * 3.0)
-//        
-//        scrollView.contentSize = CGSize(width: view.frame.width, height: height)
-//        
-//    }
 
 }
